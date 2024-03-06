19cedc7f835e3fdb4d403511a218915d2de84428
ethandmd <ethandmd@protonmail.com>
2024-03-06 14:49:53 -0800

    Per thread loop unrolling.
    Unroll the main loop in the mapper function so that each thread processes its thread chunk in N partitions (in this case thirds).
    This resulted in a 1-1.5 second speedup on my laptop.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 // Read data. Calculate min/ave/max. Print results alphabetically by station name.
 use libc::{mmap, off_t, MAP_PRIVATE, PROT_READ};
 use std::collections::HashMap;
-//use std::collections::HashMap;
 use std::fs::File;
 use std::hash::{BuildHasherDefault, Hasher};
 use std::io::Error;
@@ -79,6 +78,15 @@ impl Hasher for LilFnvHasher {
 type LilFnvHashBuilder = BuildHasherDefault<LilFnvHasher>;
 type LilFnvHashMap<K, V> = HashMap<K, V, LilFnvHashBuilder>;
 
+// Get indices [a,b) into a byte slice to get a chunk of size n_1 = b - a where
+// n_1 >= n based on the next instance of the given delimiter.
+#[inline]
+fn chunker(start: usize, n: usize, delim: u8, b: &[u8]) -> (usize, usize) {
+    let end = (start + n).min(b.len());
+    let pad = b[end..].iter().position(|c| c == &delim).unwrap_or(0);
+    (start, end + pad)
+}
+
 #[inline]
 fn merge<'a>(
     mut left: LilFnvHashMap<&'a [u8], WeatherEntry>,
@@ -96,22 +104,90 @@ fn merge<'a>(
 
 #[inline]
 fn mapper(start: usize, end: usize, mmap_bytes: &[u8]) -> LilFnvHashMap<&[u8], WeatherEntry> {
+    let thirds = (end - start) / 3;
+    let (start1, end1) = chunker(start, thirds, b'\n', mmap_bytes);
+    let (start2, end2) = chunker(end1 + 1, thirds, b'\n', mmap_bytes);
+    let (start3, end3) = chunker(end2 + 1, thirds, b'\n', mmap_bytes);
+    let mut lines1 = mmap_bytes[start1..end1].split(|c| c == &b'\n');
+    let mut lines2 = mmap_bytes[start2..end2].split(|c| c == &b'\n');
+    let mut lines3 = mmap_bytes[start3..end3].split(|c| c == &b'\n');
     let mut map: LilFnvHashMap<&[u8], WeatherEntry> = LilFnvHashMap::default();
-    for line in mmap_bytes[start..end].split(|c| c == &b'\n') {
-        if line.is_empty() {
-            continue;
+    loop {
+        if (&mut lines1).peekable().peek().is_none() {
+            break;
         }
-        let mut delim = line.split(|c| c == &b';');
-        //let name = std::str::from_utf8(delim.next().unwrap()).unwrap();
-        let name = delim.next().unwrap();
+        if (&mut lines2).peekable().peek().is_none() {
+            break;
+        }
+        if (&mut lines3).peekable().peek().is_none() {
+            break;
+        }
+        let line1 = match lines1.next() {
+            Some(l) => l,
+            None => break,
+        };
+        let line2 = match lines2.next() {
+            Some(l) => l,
+            None => break,
+        };
+        let line3 = match lines3.next() {
+            Some(l) => l,
+            None => break,
+        };
+        let mut delim1 = line1.split(|c| c == &b';');
+        let mut delim2 = line2.split(|c| c == &b';');
+        let mut delim3 = line3.split(|c| c == &b';');
+        let name1 = delim1.next().unwrap();
+        let name2 = delim2.next().unwrap();
+        let name3 = delim3.next().unwrap();
         // SAFETY: We already know the inputs are valid utf8.
-        let tmp = delim.next().unwrap();
-        let temp = unsafe { std::str::from_utf8_unchecked(tmp) }
+        let temp1 = unsafe { std::str::from_utf8_unchecked(delim1.next().unwrap()) }
+            .parse::<f64>()
+            .unwrap();
+        let temp2 = unsafe { std::str::from_utf8_unchecked(delim2.next().unwrap()) }
+            .parse::<f64>()
+            .unwrap();
+        let temp3 = unsafe { std::str::from_utf8_unchecked(delim3.next().unwrap()) }
+            .parse::<f64>()
+            .unwrap();
+        let entry = map.entry(name1).or_default();
+        (*entry).update(temp1);
+        let entry = map.entry(name2).or_default();
+        (*entry).update(temp2);
+        let entry = map.entry(name3).or_default();
+        (*entry).update(temp3);
+    }
+
+    while let Some(line) = lines1.next() {
+        let mut delim = line.split(|c| c == &b';');
+        let name = delim.next().unwrap();
+        let temp = unsafe { std::str::from_utf8_unchecked(delim.next().unwrap()) }
             .parse::<f64>()
             .unwrap();
         let entry = map.entry(name).or_default();
         (*entry).update(temp);
     }
+
+    while let Some(line) = lines2.next() {
+        let mut delim = line.split(|c| c == &b';');
+        let name = delim.next().unwrap();
+        let temp = unsafe { std::str::from_utf8_unchecked(delim.next().unwrap()) }
+            .parse::<f64>()
+            .unwrap();
+        let entry = map.entry(name).or_default();
+        (*entry).update(temp);
+    }
+
+    while let Some(line) = lines3.next() {
+        let mut delim = line.split(|c| c == &b';');
+        let name = delim.next().unwrap();
+        let temp = unsafe { std::str::from_utf8_unchecked(delim.next().unwrap()) }
+            .parse::<f64>()
+            .unwrap();
+        let entry = map.entry(name).or_default();
+        (*entry).update(temp);
+    }
+
     map
 }
 
@@ -142,7 +218,7 @@ fn main() -> std::io::Result<()> {
     if m.is_null() {
         return Err(Error::from_raw_os_error(-1));
     }
-
+    let mmap_size = mmap_size - 1; // TODO: hack to avoid getting last '\n' in a chunk.
     let bytes_ptr: *mut u8 = m.cast(); // Take ownership of the mmap ptr so we don't manually drop.
     let mmap_bytes = unsafe { slice::from_raw_parts(bytes_ptr, mmap_size) };
     let mut start = 0;
@@ -154,8 +230,6 @@ fn main() -> std::io::Result<()> {
             .position(|c| c == &b'\n')
             .unwrap_or(0);
         let end = end + pad;
-        #[cfg(debug_assertions)]
-        println!("Starting thread {} at bytes[{}..{}]", _id, start, end);
         //thread::scope(|s| {
         let t = thread::Builder::new().name(_id.to_string());
         handles.push(t.spawn(move || mapper(start, end, mmap_bytes)).unwrap());
